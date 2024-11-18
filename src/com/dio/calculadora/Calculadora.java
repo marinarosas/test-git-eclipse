43006a9df9c4a4d3cb9b3589a46b4bd4a0da8a07
Marina Rosas Rachid Jaudy <marinarosasrachidjaudy@MBP-de-Marina.lan>
2024-11-17 21:31:39 -0300

    calculator creation

---
+++ src/com/dio/calculadora/Calculadora.java
@@ -0,0 +1,46 @@
+package com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner (System.in);
+		
+		float a, b;
+		
+		System.out.println("Digite o primeiro valor: ");
+		a = scan.nextInt();
+		System.out.println("Digite o segundo valor: ");
+		b = scan.nextInt();
+		
+		float soma = soma(a,b);
+		float subtracao = subtracao(a,b);
+		float multiplicacao = multiplicacao(a,b);
+		float divisao = divisao(a,b);
+		
+		System.out.println("soma: " + soma);
+		System.out.println("subtração: " + subtracao);
+		System.out.println("multiplicação: " + multiplicacao);
+		System.out.println("divisão: " + divisao);
+			
+	}
+	
+	public static float soma (float a, float b) {
+		return a + b;
+	}
+	
+	public static float subtracao (float a, float b) {
+		return a - b;
+	}
+	
+	public static float divisao (float a, float b) {
+		return a / b;
+	}
+	public static float multiplicacao (float a, float b) {
+		return a * b;
+	}
+	
+	
+
+}
